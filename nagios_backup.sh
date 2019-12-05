894d735e66a92d9c09d5e0c6438ad4fee643d384
Fabian Baena <fabbaena@gmail.com>
2019-12-05 16:06:14 +0000

    added file

--- nagios_backup.sh
+++ nagios_backup.sh
@@ -2,7 +2,7 @@
 
 BACKUPDIR=/var/backup
 DATE=$(date +%Y%m%d)
-if [ ! -d "${BACKUPDIR}"]; then
+if [ ! -d "${BACKUPDIR}" ]; then
     mkdir -p ${BACKUPDIR}
 fi
 find ${BACKUPDIR} -type f -mtime +30 -exec rm -f {} \;
