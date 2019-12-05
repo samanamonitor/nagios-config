a01455c5b2cc7fa36226c99ef0fb27900c93051e
Fabian Baena <fabbaena@gmail.com>
2019-12-05 16:01:36 +0000

    added file

---
+++ nagios_backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BACKUPDIR=/var/backup
+DATE=$(date +%Y%m%d)
+if [ ! -d "${BACKUPDIR}"]; then
+    mkdir -p ${BACKUPDIR}
+fi
+find ${BACKUPDIR} -type f -mtime +30 -exec rm -f {} \;
+tar -czvf /var/backup/${DATE}nagios.tgz
